7eb4870b31b77196bb90d2d2382f64f355f28bfc
Christopher Wood <cjwood032@gmail.com>
2018-07-12 16:12:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,5 +7,9 @@ attr_accessor :name, :grade
   end #def initialize
 ###################################
   def self.create_table
+    sql= <<-TEXT
+      CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
+    TEXT
+    DB[:conn].execute(sql)
   end #end the create table method
 end
